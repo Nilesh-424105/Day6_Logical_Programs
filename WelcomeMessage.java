ac62b973377d856a920bc330304a563f7f51298f
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-07 14:11:19 +0530

    ADDED: Welcome_Message

---
+++ WelcomeMessage.java
@@ -0,0 +1,11 @@
+package com.logical.programs;
+
+/*
+ *  Welcome Message
+ */
+public class WelcomeMessage {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Day6 Logical Programs");
+	}
+
+}
